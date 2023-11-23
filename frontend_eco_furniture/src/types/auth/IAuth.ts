0ca6953ad21e36b40f7bb2facf3ca3abf7a21bf9
songkidong <141198018+songkidong@users.noreply.github.com>
2023-11-23 12:44:07 +0900

    Revert "업데이트_로그인&회원가입"
    This reverts commit 8eee76881e335f0bff5f4e0bd59a4a3c2ccf436e.

--- frontend_eco_furniture/src/types/auth/IAuth.ts
+++
@@ -1,9 +0,0 @@
-// IAuth 인터페이스
-
-import IUser from "./IUser";
-
-// 유저 상태정보 : 로그인여부
-export default interface IAuth {
-  isLoggedIn: boolean; // 로그인상태(true/false)
-  user?: IUser | null; // 유저객체
-}
